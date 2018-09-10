7aec5f40fb08f73b1090843cf1594dcab07ef356
ethanryan <ethanryan@gmail.com>
2018-09-10 16:03:25 -0400

    in tasks_controller, added list_id as a permitted params, along with name... not making list_id a required params for now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -88,6 +88,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :completed)
+      params.require(:task).permit(:name, :completed, :list_id)
     end
 end
