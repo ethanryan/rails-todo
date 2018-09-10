3fafe2833446f0c803db66f5e270a817d8c5dca6
ethanryan <ethanryan@gmail.com>
2018-09-10 13:52:07 -0400

    added custom routes to tasks_controller actions, done and incomplete, and make root route to tasks#incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: "tasks#index"
-  resources :tasks
+  # root to: "tasks#index"
+  root to: "tasks#incomplete"
+
+  # resources :tasks
+  get "/tasks/done", to: "tasks#done"
+  get "/tasks/incomplete", to: "tasks#incomplete"
+  resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  # get 'exit', to: 'sessions#destroy', as: :logout
+
 end
